5ec80127956d207ed928a60cb90949afbe61ccce
Landon Bouma <landonb@users.noreply.github.com>
2025-01-22 15:29:56 -0600

    Feature: Add Neovim config, using lazy.nvim plugin loader

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- vim:tw=0:ts=2:sw=2:et:ai:ft=lua
+-- Author: Landon Bouma <https://tallybark.com/>
+-- Project: https://github.com/DepoXy/depoxy#🍯
+-- License: MIT
+
+-- CXREF:
+-- ~/.config/nvim/lua/config/lazy.lua
+-- ~/.config/nvim/lua/plugins/spec.lua
+require("config.lazy")
+
