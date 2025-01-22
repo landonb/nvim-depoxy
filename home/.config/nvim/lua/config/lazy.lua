716eeb04882e2f4e00b9f790ea17b6e9d7c7358e
Landon Bouma <landonb@users.noreply.github.com>
2025-01-22 15:31:32 -0600

    Docs: OMR: Update comments re: lazy.nvim config

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -5,9 +5,27 @@
 
 -- COPYD: https://lazy.folke.io/installation
 
+-- USAGE: Create plugin specs in ~/.config/nvim/lua/plugins/
+--
+-- - Each file returns a table with the plugins to install.
+--
+-- - CXREF:
+--
+--    ~/.config/nvim/lua/plugins/spec.lua
+--    ~/.config/nvim/lua/plugins/spec-dubs.lua
+--
+-- REFER: ~/.config/nvim per: :echo stdpath('config')
+
+-- SETUP: After installation, run:
+--
+--    :checkhealth lazy
+
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  -- REFER: On macOS (& prob. Linux), installs to:
+  --   ~/.local/share/nvim/lazy/lazy.nvim/
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
@@ -35,6 +53,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
+  -- REFER: https://github.com/ntk148v/habamax.nvim
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
